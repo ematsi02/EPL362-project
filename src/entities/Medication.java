4e3bf722854350156463dbdc6337d8d4e2c0a6a1
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-08 19:19:49 +0300

    Remove constructors

--- src/entities/Medication.java
+++ src/entities/Medication.java
@@ -18,19 +18,6 @@ public class Medication implements java.io.Serializable   {
 	public int MaxDose;
 	public static ArrayList<String> columnNames = fillColumnNames();
 
-	public Medication() {
-		new Medication(0, null, null, null, null, 0);
-	}
-
-	public Medication(int medicationID, String brand, String name, String description, String knownSideEffects,
-			int maxDose) {
-		this.MedicationID = medicationID;
-		this.Brand = brand;
-		this.Name = name;
-		this.Description = description;
-		this.KnownSideEffects = knownSideEffects;
-		this.MaxDose = maxDose;
-	}
 	public List<Medication> convertRsToList(ResultSet rs) throws SQLException{
 		List<Medication> Medication=new ArrayList<Medication>();
 		while(rs.next()) {
