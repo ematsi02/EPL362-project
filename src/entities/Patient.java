38ac567eafaca6d1827b93c26e0243d5a1a1da15
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-08 19:13:24 +0300

    Remove Constructors

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -26,29 +26,6 @@ public class Patient implements java.io.Serializable  {
 	public boolean DeadReadOnly;
 	public static ArrayList<String> columnNames = fillColumnNames();
 	
-	public Patient() {
-		new Patient(null, null, null, null, 0, null, null, 0, false, false, null, false, false);
-	}
-	
-	public Patient(String PatientID, String Password, String Name, String Surname, int Phone, String Email,
-			String Address, int NumOfIncidents, boolean SelfHarmRisk, boolean OthersHarmRisk, String RiskStatus,
-			boolean ChangedByPatient, boolean DeadReadOnly) {
-		this.PatientID = PatientID;
-		this.Password = Password;
-		this.Name = Name;
-		this.Surname = Surname;
-		this.Phone = Phone;
-		this.Email = Email;
-		this.Address = Address;
-		this.NumOfIncidents = NumOfIncidents;
-		this.SelfHarmRisk = SelfHarmRisk;
-		this.OthersHarmRisk = OthersHarmRisk;
-		this.RiskStatus = RiskStatus;
-		this.ChangedByPatient = ChangedByPatient;
-		this.DeadReadOnly = DeadReadOnly;
-		this.fillColumnNames();
-	}
-	
 	public List<Patient> convertRsToList(ResultSet rs) throws SQLException{
 		List<Patient> Patients=new ArrayList<Patient>();
 		while(rs.next()) {
