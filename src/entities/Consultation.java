1909f592b49341ce9076b9e038047053d9d5e161
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-08 19:13:45 +0300

    Remove Constructors

--- src/entities/Consultation.java
+++ src/entities/Consultation.java
@@ -21,23 +21,6 @@ public class Consultation implements java.io.Serializable   {
 	public int MedicalRecordUpdated;
 	public int TreatmentID;
 
-	public Consultation() {
-		new Consultation(0, null, null, null, null, null, null, 0, 0, 0);
-	}
-
-	public Consultation(int consultationID, String patientID, String staffID, String subject, String dateBooked,
-			String date, String time, int attended, int medicalRecordUpdated, int treatmentID) {
-		this.ConsultationID = consultationID;
-		this.PatientID = patientID;
-		this.StaffID = staffID;
-		this.Subject = subject;
-		this.DateBooked = dateBooked;
-		this.Date = date;
-		this.Time = time;
-		this.Attended = attended;
-		this.MedicalRecordUpdated = medicalRecordUpdated;
-		this.TreatmentID = treatmentID;
-	}
 	public List<Consultation> convertRsToList(ResultSet rs) throws SQLException{
 		List<Consultation> Consultation=new ArrayList<Consultation>();
 		while(rs.next()) {
