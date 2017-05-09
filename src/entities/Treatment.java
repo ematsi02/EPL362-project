2a6235222b94a16ad4f64095dfa30f95003a148b
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-09 17:15:38 +0300

    Add functionality about Table Model

--- src/entities/Treatment.java
+++ src/entities/Treatment.java
@@ -5,8 +5,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Treatment implements java.io.Serializable   {
-	
+import javax.swing.table.DefaultTableModel;
+
+public class Treatment implements java.io.Serializable {
+
 	private static final long serialVersionUID = 1L;
 	public int TreatmentID;
 	public String PatientID;
@@ -17,19 +19,57 @@ public class Treatment implements java.io.Serializable   {
 	public String StaffID;
 	public static ArrayList<String> columnNames = fillColumnNames();
 
-	public List<Treatment> convertRsToList(ResultSet rs) throws SQLException{
-		List<Treatment> Treatment=new ArrayList<Treatment>();
-		while(rs.next()) {
-			Treatment treatment=new Treatment();
-			treatment.TreatmentID=rs.getInt("TreatmentID");
-			treatment.PatientID=rs.getString("PatientID");
-			treatment.StartDate=rs.getString("StartDate");
-			treatment.EndDate=rs.getString("EndDate");
-			treatment.Diagnosis=rs.getString("Diagnosis");
-			treatment.Description=rs.getString("Description");
-			treatment.StaffID=rs.getString("StaffID");
+	public String getfield(int i) {
+		switch (i) {
+		case 0:
+			return Integer.toString(TreatmentID);
+		case 1:
+			return PatientID;
+		case 2:
+			return StartDate;
+		case 3:
+			return EndDate;
+		case 4:
+			return Diagnosis;
+		case 5:
+			return Description;
+		default:
+			return StaffID;
+		}
+	}
+
+	public static DefaultTableModel buildTableModel(List<Treatment> list, ArrayList<String> columnNames)
+			throws SQLException {
+		int columnCount = columnNames.size();
+		String[] columns = new String[columnCount];
+		for (int column = 0; column < columnCount; column++) {
+			columns[column] = columnNames.get(column);
+		}
+		int i = 0;
+		String[][] data = new String[list.size()][columnCount];
+		while (i < list.size()) {
+			Treatment t = list.get(i);
+			for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+				data[i][columnIndex] = t.getfield(columnIndex);
+			}
+			i++;
+		}
+		return new DefaultTableModel(data, columns);
+	}
+
+	public List<Treatment> convertRsToList(ResultSet rs) throws SQLException {
+		List<Treatment> Treatment = new ArrayList<Treatment>();
+		while (rs.next()) {
+			Treatment treatment = new Treatment();
+			treatment.TreatmentID = rs.getInt("TreatmentID");
+			treatment.PatientID = rs.getString("PatientID");
+			treatment.StartDate = rs.getString("StartDate");
+			treatment.EndDate = rs.getString("EndDate");
+			treatment.Diagnosis = rs.getString("Diagnosis");
+			treatment.Description = rs.getString("Description");
+			treatment.StaffID = rs.getString("StaffID");
 			Treatment.add(treatment);
-		} 
+		}
 		return Treatment;
 	}
 
