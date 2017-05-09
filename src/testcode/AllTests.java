aeac9646fb189e585a7505c9060a44bbafea1a01
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-09 20:04:05 +0300

    All Tests - Test Suit

---
+++ src/testcode/AllTests.java
@@ -0,0 +1,11 @@
+package testcode;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PatientTest.class, StaffTest.class })
+public class AllTests {
+
+}
