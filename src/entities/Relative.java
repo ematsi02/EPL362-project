84aab1ee990b6e56f71ceb43afc55cd9010c2562
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-09 17:46:13 +0300

    Add functionality about Table Model

--- src/entities/Relative.java
+++ src/entities/Relative.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Relative implements java.io.Serializable  {
+import javax.swing.table.DefaultTableModel;
+
+public class Relative implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 	public int RelativeID;
@@ -17,23 +19,62 @@ public class Relative implements java.io.Serializable  {
 	public String Address;
 	public String Relationship;
 	public static ArrayList<String> columnNames = fillColumnNames();
-	
-	public List<Relative> convertRsToRelatList(ResultSet rs) throws SQLException{
-		List<Relative> Relatives=new ArrayList<Relative>();
-		while(rs.next()) {
-			Relative relative=new Relative();
-			relative.RelativeID=rs.getInt("RelativeID");
-			relative.PatientID=rs.getString("PatientID");
-		   relative.Name=rs.getString("Name");
-		   relative.Surname=rs.getString("Surname");
-		   relative.Phone=rs.getInt("Phone");
-		   relative.Email=rs.getString("Email");
-		   relative.Address=rs.getString("Address");
-		   relative.Relationship=rs.getString("Relationship");
 
+	public String getfield(int i) {
+		switch (i) {
+		case 0:
+			return Integer.toString(RelativeID);
+		case 1:
+			return PatientID;
+		case 2:
+			return Name;
+		case 3:
+			return Surname;
+		case 4:
+			return Integer.toString(Phone);
+		case 5:
+			return Email;
+		case 6:
+			return Address;
+		default:
+			return Relationship;
+		}
+	}
 
-		  Relatives.add(relative);
-		} 
+	public static DefaultTableModel buildTableModel(List<Relative> list, ArrayList<String> columnNames)
+			throws SQLException {
+		int columnCount = columnNames.size();
+		String[] columns = new String[columnCount];
+		for (int column = 0; column < columnCount; column++) {
+			columns[column] = columnNames.get(column);
+		}
+		int i = 0;
+		String[][] data = new String[list.size()][columnCount];
+		while (i < list.size()) {
+			Relative t = list.get(i);
+			for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+				data[i][columnIndex] = t.getfield(columnIndex);
+			}
+			i++;
+		}
+		return new DefaultTableModel(data, columns);
+	}
+
+	public List<Relative> convertRsToRelatList(ResultSet rs) throws SQLException {
+		List<Relative> Relatives = new ArrayList<Relative>();
+		while (rs.next()) {
+			Relative relative = new Relative();
+			relative.RelativeID = rs.getInt("RelativeID");
+			relative.PatientID = rs.getString("PatientID");
+			relative.Name = rs.getString("Name");
+			relative.Surname = rs.getString("Surname");
+			relative.Phone = rs.getInt("Phone");
+			relative.Email = rs.getString("Email");
+			relative.Address = rs.getString("Address");
+			relative.Relationship = rs.getString("Relationship");
+
+			Relatives.add(relative);
+		}
 		return Relatives;
 	}
 
