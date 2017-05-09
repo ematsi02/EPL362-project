9e11bae6912ccaded71eaf49f7e7d1428a658020
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-09 15:43:56 +0300

    Update Patient.java

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Patient implements java.io.Serializable  {
+import javax.swing.table.DefaultTableModel;
+
+public class Patient implements java.io.Serializable {
 
 	/**
 	 * 
@@ -25,31 +27,79 @@ public class Patient implements java.io.Serializable  {
 	public int ChangedByPatient;
 	public int DeadReadOnly;
 	public static ArrayList<String> columnNames = fillColumnNames();
-	
-	public List<Patient> convertRsToList(ResultSet rs) throws SQLException{
-		List<Patient> Patients=new ArrayList<Patient>();
-		while(rs.next()) {
-			Patient patient=new Patient();
-			patient.PatientID=rs.getString("PatientID");
-		   patient.Password=rs.getString("Password");
-		   patient.Name=rs.getString("Name");
-		   patient.Surname=rs.getString("Surname");
-		   patient.Phone=rs.getInt("Phone");
-		   patient.Email=rs.getString("Email");
-		   patient.Address=rs.getString("Address");
-		   patient.NumOfIncidents=rs.getInt("NumOfIncidents");
-		   patient.SelfHarmRisk=rs.getInt("SelfHarmRisk");
-		   patient.OthersHarmRisk=rs.getInt("OthersHarmRisk");
-		   patient.RiskStatus=rs.getString("RiskStatus");
-		   patient.ChangedByPatient=rs.getInt("ChangedByPatient");
-		   patient.DeadReadOnly=rs.getInt("DeadReadOnly");
 
-		  Patients.add(patient);
-		} 
+	public String getfield(int i) {
+		switch (i) {
+		case 0:
+			return PatientID;
+		case 1:
+			return Password;
+		case 2:
+			return Name;
+		case 3:
+			return Surname;
+		case 4:
+			return Integer.toString(Phone);
+		case 5:
+			return Email;
+		case 6:
+			return Address;
+		case 7:
+			return Integer.toString(SelfHarmRisk);
+		case 8:
+			return Integer.toString(OthersHarmRisk);
+		case 9:
+			return RiskStatus;
+		case 10:
+			return Integer.toString(ChangedByPatient);
+		default:
+			return Integer.toString(DeadReadOnly);
+		}
+	}
+
+	public static DefaultTableModel buildTableModel(List<Patient> list, ArrayList<String> columnNames)
+			throws SQLException {
+		int columnCount = columnNames.size();
+		String[] columns = new String[columnCount];
+		for (int column = 0; column < columnCount; column++) {
+			columns[column] = columnNames.get(column);
+		}
+		int i = 0;
+		String[][] data = new String[list.size()][columnCount];
+		while (i < list.size()) {
+			Patient t = list.get(i);
+			for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+				data[i][columnIndex] = t.getfield(columnIndex);
+			}
+			i++;
+		}
+		return new DefaultTableModel(data, columns);
+	}
+
+	public List<Patient> convertRsToList(ResultSet rs) throws SQLException {
+		List<Patient> Patients = new ArrayList<Patient>();
+		while (rs.next()) {
+			Patient patient = new Patient();
+			patient.PatientID = rs.getString("PatientID");
+			patient.Password = rs.getString("Password");
+			patient.Name = rs.getString("Name");
+			patient.Surname = rs.getString("Surname");
+			patient.Phone = rs.getInt("Phone");
+			patient.Email = rs.getString("Email");
+			patient.Address = rs.getString("Address");
+			patient.NumOfIncidents = rs.getInt("NumOfIncidents");
+			patient.SelfHarmRisk = rs.getInt("SelfHarmRisk");
+			patient.OthersHarmRisk = rs.getInt("OthersHarmRisk");
+			patient.RiskStatus = rs.getString("RiskStatus");
+			patient.ChangedByPatient = rs.getInt("ChangedByPatient");
+			patient.DeadReadOnly = rs.getInt("DeadReadOnly");
+
+			Patients.add(patient);
+		}
 		return Patients;
 	}
-	
-	private static ArrayList<String> fillColumnNames(){
+
+	private static ArrayList<String> fillColumnNames() {
 		ArrayList<String> columnNames = new ArrayList<String>();
 		columnNames.add("PatientID");
 		columnNames.add("Password");
