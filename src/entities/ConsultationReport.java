eb3b696dd76f16077ccc3bf42834469b12a23468
ematsi02 <ematsi02@cs.ucy.ac.cy>
2017-05-09 15:14:00 +0300

    Update ConsultationReport.java

--- src/entities/ConsultationReport.java
+++ src/entities/ConsultationReport.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  * This object is used for the results of the Consultation Reports (Attended
  * Daily Consultations, Attended General Consultations, Non Attended Daily
@@ -22,44 +24,35 @@ public class ConsultationReport implements java.io.Serializable {
 	public String Consultation_Subject;
 	public String Consultation_Date;
 	public String Consultation_Time;
-	public int Patient_PatientID;
+	public String Patient_PatientID;
 	public String Patient_Name;
 	public String Patient_Surname;
-	public int Staff_StaffID;
+	public String Staff_StaffID;
 	public String Staff_Name;
 	public String Staff_Surname;
 	public int Consultation_MedicalRecordUpdated;
-	public static ArrayList<String> columnNames = fillColumnNames();
+	public ArrayList<String> columnNames = fillColumnNames();
 
-	public List<ConsultationReport> convertRsToList(ResultSet rs) throws SQLException {
-		List<ConsultationReport> ReportList = new ArrayList<ConsultationReport>();
-		while (rs.next()) {
-			ConsultationReport cr = new ConsultationReport();
-			cr.Consultation_ConsultationID = rs.getInt("ConsultationID");
-			cr.Consultation_Subject = rs.getString("Subject");
-			cr.Consultation_Date = rs.getString("Date");
-			cr.Consultation_Time = rs.getString("Time");
-			cr.Patient_PatientID = rs.getInt("PatientID");
-			cr.Patient_Name = rs.getString("Name");
-			cr.Patient_Surname = rs.getString("Surname");
-			cr.Staff_StaffID = rs.getInt("StaffID");
-			cr.Staff_Name = rs.getString("Name");
-			cr.Staff_Surname = rs.getString("Surname");
-			cr.Consultation_MedicalRecordUpdated = rs.getInt("MedicalRecordUpdated");
-
-			ReportList.add(cr);
-		}
-		return ReportList;
+	public String toString(){
+		return ("I am toString of Consultation Report\n");
 	}
-	
+
 	public String getfield(int i){
 		switch (i){
 		case 0: return ((Integer)Consultation_ConsultationID).toString();
-		case 1: return Consultation_Date;
-		// TODO continue with the rest columns
+		case 1: return Consultation_Subject;
+		case 2: return Consultation_Date;
+		case 3: return Consultation_Time;
+		case 4: return Patient_PatientID;
+		case 5: return Patient_Name;
+		case 6: return Patient_Surname;
+		case 7: return Staff_StaffID;
+		case 8: return Staff_Name;
+		case 9: return Staff_Surname;
+		default: return ((Integer)Consultation_MedicalRecordUpdated).toString();
 		}
 	}
-
+	
 	public static DefaultTableModel buildTableModel(List<ConsultationReport> list, ArrayList<String> columnNames)
 			throws SQLException {
 		int columnCount = columnNames.size();
@@ -79,6 +72,27 @@ public class ConsultationReport implements java.io.Serializable {
 		return new DefaultTableModel(data, columns);
 	}
 	
+	public List<ConsultationReport> convertRsToList(ResultSet rs) throws SQLException {
+		List<ConsultationReport> ReportList = new ArrayList<ConsultationReport>();
+		while (rs.next()) {
+			ConsultationReport cr = new ConsultationReport();
+			cr.Consultation_ConsultationID = (int) rs.getInt("ConsultationID");
+			cr.Consultation_Subject = rs.getString("Subject");
+			cr.Consultation_Date = rs.getString("Date");
+			cr.Consultation_Time = rs.getString("Time");
+			cr.Patient_PatientID = rs.getString("PatientID");
+			cr.Patient_Name = rs.getString("Name");
+			cr.Patient_Surname = rs.getString("Surname");
+			cr.Staff_StaffID = rs.getString("StaffID");
+			cr.Staff_Name = rs.getString("Name");
+			cr.Staff_Surname = rs.getString("Surname");
+			cr.Consultation_MedicalRecordUpdated = rs.getInt("MedicalRecordUpdated");
+
+			ReportList.add(cr);
+		}
+		return ReportList;
+	}
+
 	private static ArrayList<String> fillColumnNames() {
 		ArrayList<String> columnNames = new ArrayList<String>();
 		columnNames.add("ConsultationID");
